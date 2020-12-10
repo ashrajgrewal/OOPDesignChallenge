7797f1bba7d0eb2ecf4f5445cfc3b85695b8edb3
Ashraj Grewal <ashraj.grewal10@gmail.com>
2020-12-10 01:32:44 -0800

    Create Mercedes.py

---
+++ Mercedes.py
@@ -0,0 +1,9 @@
+#OOP DESIGN CHALLENGE
+class Mercedes:
+    def __init__(self, year, model, doors, color):
+        self.year = year
+        self.model = model
+        self.doors = doors
+        self.color = color
+        
+
