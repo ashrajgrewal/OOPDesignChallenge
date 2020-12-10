3c83e621c1c1e522b9a6b614a2176ab5ef889eae
Ashraj Grewal <ashraj.grewal10@gmail.com>
2020-12-10 01:39:12 -0800

    Update Mercedes.py

--- Mercedes.py
+++ Mercedes.py
@@ -3,25 +3,24 @@ class basicCarSpec:
     def __init__(self, year, model, color):
         self.year = year
         self.model = model
-        self.doors = doors
         self.color = color
-    #Creating a private function to get the spec of the car object instantiated.
-    def get_spec(self):
-        return self.year
-        return self.model
-        return self.color
+ #Creating a public function to get the spec of the car object instantiated.
+def get_spec(self):
+    return self.year
+    return self.model
+    return self.color
     #Now I am creating another class for a non standard benz, an AMG benz
     #This inherits from the basicCarSpec class because everuy car will have those things.
-class C63AMG(BasicCarSpec):
+class C63AMG(basicCarSpec):
     def __init__(self, seats, exhaust):
         self.seats = seats
         self.exhaust = exhaust
-class gWagon(BasicCarSpec):
+class gWagon(basicCarSpec):
     def __init__(self, trim, top):
         self.trim = trim
         self.top = top
-class maybach(BasicCarSpec):
+class maybach(basicCarSpec):
     def __init__(self, wheelbase, use):
         self.wheelbase = wheelbase
         self.use = use
-    
+#These 3 previous classes are for other mercedes models that can be instantiated.
