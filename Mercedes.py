5346856393cc141f165658cd6cbfc2a06212175d
Ashraj Grewal <ashraj.grewal10@gmail.com>
2020-12-10 16:37:01 -0800

    Update Mercedes.py

--- Mercedes.py
+++ Mercedes.py
@@ -17,9 +17,9 @@ class C63AMG(basicCarSpec):
     def get_spec(self):
         print(f"{self.year} {self.model} {self.color} {self.seats} {self.exhaust}")
     def get_seats(self):
-        print(self.seats)
+        print(f"You opted for {self.seats}!")
     def get_exhaust(self):
-        print(self.exhaust)
+        print(f"You opted for {self.exhaust}!")
 class gWagon(basicCarSpec):
     def __init__(self, year, model, color, trim, top):
         self.trim = trim
@@ -32,15 +32,17 @@ class maybach(basicCarSpec):
     def __init__(self, year, model, color, wheelbase, use):
         self.wheelbase = wheelbase
         self.use = use
+    def get_spec(self):
+        print(f"{self.year} {self.model} {self.color} {self.wheelbase} {self.use}")
     def get_wheelbase(self):
         print(self.wheelbase)
     def get_use(self):
         print(self.use)
 #These 3 previous classes are for other mercedes models that can be instantiated.
-myCar = basicCarSpec(2020, "sedan", "white")
-myCar.get_spec()
+#myCar = basicCarSpec(2020, "sedan", "white")
+#myCar.get_spec()
 
-myAMG = C63AMG(2020, "coupe", "black", "sport seats", "sport exhaust")
-myAMG.get_spec()
-myAMG.get_seats()
-myAMG.get_exhaust()
+#myAMG = C63AMG(2020, "coupe", "black", "sport seats", "sport exhaust")
+#myAMG.get_spec()
+#myAMG.get_seats()
+#myAMG.get_exhaust()
